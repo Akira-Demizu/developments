fefb03cbb2c54ed7ab7ee0f548f3caadb2afb3d1
Akira Demizu <a.demizu@saver.jp>
2022-02-01 11:10:04 +0900

    #3 vue組み込み

--- laravel8_vue3/server/resources/js/app.js
+++ laravel8_vue3/server/resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+import TestVue from './components/TestVue.vue';
+
+const app = createApp({})
+app.component('test-vue', TestVue);
+app.mount('#app')
---
+++ laravel8_vue3/server/resources/js/components/TestVue.vue
@@ -0,0 +1,15 @@
+<template>
+    Hello Vue !!
+</template>
+<script>
+import { onMounted } from 'vue'
+export default ({
+    setup() {
+        console.log("start vue")
+
+        onMounted(() => {
+            console.log('start mounted !')
+        })
+    },
+})
+</script>
